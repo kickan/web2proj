c81d5379254111f26a477d100a1808efbadb6eae
Kickan <kristina_ab@hotmail.com>
2022-02-22 12:10:00 +0100

    add check for error messages

--- login.php
+++ login.php
@@ -5,19 +5,27 @@ include("includes/header.php");
 #Create new user obj
 $user = new User;
 $message = "";
+$f_username = "";
+
+#Check for error message
+if (isset($_GET['error'])) {
+    if ($_GET['error'] == '1') {
+        $message = "Du måste vara inloggad för att besöka denna sida.";
+    }
+}
 
 #Check if user wants to login
-if(isset($_POST['logInBtn'])){
+if (isset($_POST['logInBtn'])) {
     $username = $_POST['username'];
-    $pass = $_POST['password'];
+    $password = $_POST['password'];
 
-    if($user->loginUser($username, $password)){
+    if ($user->loginUser($username, $password)) {
         $_SESSION['k_user'] = $username;
         header("Location:admin.php");
-    }else{
-        $message = "Felaktigt användarnamn/lösenord!";
+    } else {
+        $f_username = $username;
+        $message = "Felaktigt användarnamn eller lösenord!";
     }
-
 }
 ?>
 
@@ -25,10 +33,11 @@ if(isset($_POST['logInBtn'])){
     <h1>Logga in</h1>
     <form action="login.php" method="post">
         <label for="username">Användarnamn: </label><br>
-        <input type="text" id="username" name="username"><br>
+        <input type="text" id="username" name="username" value="<?= $f_username ?>"><br>
         <label for="password">Lösenord: </label><br>
         <input type="password" id="password" name="password"><br>
         <input type="submit" name="logInBtn">
+        <p><?= $message ?></p>
     </form>
 </main>
 <?php
