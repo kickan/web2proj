9350740dbccc20bb0ec131841e51c7c5156d7d9a
Kickan <kristina_ab@hotmail.com>
2022-02-22 12:09:45 +0100

    Add admin nav and logged in check

--- admin.php
+++ admin.php
@@ -2,6 +2,14 @@
 $pagename = "Startsida";
 include("includes/header.php");
 
+#Create new user obj
+$user = new User;
+
+#Check if user is logged in
+if(!$user->isLoggedIn()){
+    header("Location:login.php?error=1");
+}
+
 #create new post obj
 $post = new Post;
 
@@ -10,6 +18,12 @@ $latestPosts = $post->getPosts(3);
 ?>
 <main>
     <h1>Admin</h1>
+    <nav class="admin-nav">
+        <li><a href="admin.php">Portfolio</a></li>
+        <li><a href="adminskills.php">Skills</a></li>
+        <li><a href="adminblog.php">Blogg</a></li>
+        <li><a href="adminuser.php">User</a></li>
+    </nav>
 
 </main>
 <?php
