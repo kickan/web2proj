78a175a55ac6ce354a4e2832bf4fc329ed1ffa75
Kickan <kristina_ab@hotmail.com>
2022-03-03 13:51:56 +0100

    Adjust admin nav

--- includes/adminnav.php
+++ includes/adminnav.php
@@ -1,6 +1,22 @@
-<nav class="admin-nav">
-        <li><a href="admin.php">Portfolio</a></li>
-        <li><a href="adminskills.php">Skills</a></li>
-        <li><a href="adminblog.php">Blogg</a></li>
-        <li><a href="adminuser.php">User</a></li>
-    </nav>
+<?php
+$pages = array(
+    "Portfolio" => "admin.php",
+    "Skills" => "adminskills.php",
+    "Blogg" => "adminblog.php",
+    "Användare" => "adminuser.php"
+)
+?>
+<nav>
+    <ul class="admin-nav nav-lst">
+        <?php
+        $current = basename($_SERVER["SCRIPT_FILENAME"]);
+        foreach ($pages as $page => $pageFileName) {
+            if ($current == $pageFileName) {
+        ?>
+                <li class="admin-nav-li--active"><a href="<?= $pageFileName ?>"><?= $page ?></a></li><?php
+                                                                                                    } else { ?>
+                <li><a href="<?= $pageFileName ?>"><?= $page ?></a></li><?php
+                                                                                                    }
+                                                                                                } ?>
+    </ul>
+</nav>
